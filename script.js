d068179df0d62c4996f31e9c841a2ec0f77ebd4b
DeadMall0w <100719400+DeadMall0w@users.noreply.github.com>
2023-11-27 17:15:26 +0100

    Update spanish

--- script.js
+++ script.js
@@ -75,229 +75,638 @@ const data = {
     ]
 };
 
-const voca = {
-  "vocabulaire": [
-    {
-      "frc": "Citoyenneté",
-      "espagnol": "Ciudadanía"
-    },
-    {
-      "frc": "Mondes virtuels",
-      "espagnol": "Mundos virtuales"
-    },
-    {
-      "frc": "Espace privé",
-      "espagnol": "Espacio privado"
-    },
-    {
-      "frc": "Espace publique",
-      "espagnol": "Espacio publicó"
-    },
-    {
-      "frc": "Identités",
-      "espagnol": "Identidades"
-    },
-    {
-      "frc": "Échanges",
-      "espagnol": "Intercambios"
-    },
-    {
-      "frc": "Fictions",
-      "espagnol": "Ficciones"
-    },
-    {
-      "frc": "Réalités",
-      "espagnol": "Realidades"
-    },
-    {
-      "frc": "Innovations scientifiques",
-      "espagnol": "Innovaciones científicas"
-    },
-    {
-      "frc": "Responsabilités",
-      "espagnol": "Responsabilidades"
-    },
-    {
-      "frc": "Art",
-      "espagnol": "Arte"
-    },
-    {
-      "frc": "Pouvoir",
-      "espagnol": "Poder"
-    },
-    {
-      "frc": "Territoire",
-      "espagnol": "Territorio"
-    },
-    {
-      "frc": "Mémoire",
-      "espagnol": "Memoria"
-    },
-    {
-      "frc": "Diversité",
-      "espagnol": "Diversidad"
-    },
-    {
-      "frc": "Inclusion",
-      "espagnol": "Inclusión"
-    },
-    {
-      "frc": "Un robot",
-      "espagnol": "Un robot"
-    },
-    {
-      "frc": "Une créature fantastique",
-      "espagnol": "Una criatura fantástica"
-    },
-    {
-      "frc": "Les rencontres",
-      "espagnol": "Los encuentros"
-    },
-    {
-      "frc": "Un voyage dans le temps",
-      "espagnol": "Un viaje por el tiempo"
-    },
-    {
-      "frc": "Un mélange",
-      "espagnol": "Un mezcla"
-    },
-    {
-      "frc": "Autonome",
-      "espagnol": "Autónomo"
-    },
-    {
-      "frc": "Un bras",
-      "espagnol": "Un brazo"
-    },
-    {
-      "frc": "Un chirurgien, La chirurgie",
-      "espagnol": "Un cirujano, la cirugía"
-    },
-    {
-      "frc": "Agir",
-      "espagnol": "Actuar"
-    },
-    {
-      "frc": "Arriver",
-      "espagnol": "Llegar"
-    },
-    {
-      "frc": "Un risque",
-      "espagnol": "Un riesgo"
-    },
-    {
-      "frc": "États-Unis",
-      "espagnol": "EE.UU"
-    },
-    {
-      "frc": "Retourner → … à nouveau",
-      "espagnol": "Volver → Ha vuelto a..."
-    },
-    {
-      "frc": "Réussir à, obtenir",
-      "espagnol": "Lograr"
-    },
-    {
-      "frc": "L'entraide",
-      "espagnol": "La ayuda mutua"
-    },
-    {
-      "frc": "Le foie, hépatique",
-      "espagnol": "El hígado, hepático"
-    },
-    {
-      "frc": "Un poumon, pulmonaires",
-      "espagnol": "Un pulmón, pulmonares"
-    },
-    {
-      "frc": "Un rein, rénales",
-      "espagnol": "Un riñón, renales"
-    },
-    {
-      "frc": "Trouver",
-      "espagnol": "Encuentrar"
-    },
-    {
-      "frc": "Augmentation",
-      "espagnol": "Un incremento, un aumento"
-    },
-    {
-      "frc": "Don",
-      "espagnol": "Donación"
-    },
-    {
-      "frc": "Succès",
-      "espagnol": "Éxito"
-    },
-    {
-      "frc": "5 000, 2 000",
-      "espagnol": "Cinco mil, dos mil"
-    },
-    {
-      "frc": "357 392",
-      "espagnol": "trescientos cincuenta y siete mil cuatrocientos noventa y dos"
-    },
-    {
-      "frc": "30%",
-      "espagnol": "Treinta porciento"
-    },
-    {
-      "frc": "46,9",
-      "espagnol": "Cuarenta y seis coma nueve"
-    },
-    {
-      "frc": "Choisir",
-      "espagnol": "Escoger"
-    },
-    {
-      "frc": "La peau",
-      "espagnol": "La piel"
-    },
-    {
-      "frc": "Les jambes",
-      "espagnol": "Las piernas"
-    },
-    {
-      "frc": "Un chercheur",
-      "espagnol": "Un investigador"
-    },
-    {
-      "frc": "Le manque",
-      "espagnol": "La escasez"
-    },
-    {
-      "frc": "Une arbalette",
-      "espagnol": "Una baliesta"
-    },
-    {
-      "frc": "Une récolte",
-      "espagnol": "Una cosecha"
-    },
-    {
-      "frc": "La fumée",
-      "espagnol": "El humo"
-    },
-    {
-      "frc": "Abîmé",
-      "espagnol": "Estropeado"
-    },
-    {
-      "frc": "Bon pour la santé",
-      "espagnol": "Saludable"
-    },
-    {
-      "frc": "Substances dangereuses",
-      "espagnol": "Sustancias peligrosas"
-    },
-    {
-      "frc": "Les nouveaux nés",
-      "espagnol": "Los recién nacidos"
-    },
-    {
-      "frc": "Gaz à effet de serre",
-      "espagnol": "Gases de efecto invernadero"
-    }
-  ]
+// const voca = {
+  // "vocabulaire": [
+  //   {
+  //     "frc": "Citoyenneté",
+  //     "espagnol": "Ciudadanía"
+  //   },
+  //   {
+  //     "frc": "Mondes virtuels",
+  //     "espagnol": "Mundos virtuales"
+  //   },
+  //   {
+  //     "frc": "Espace privé",
+  //     "espagnol": "Espacio privado"
+  //   },
+  //   {
+  //     "frc": "Espace publique",
+  //     "espagnol": "Espacio publicó"
+  //   },
+  //   {
+  //     "frc": "Identités",
+  //     "espagnol": "Identidades"
+  //   },
+  //   {
+  //     "frc": "Échanges",
+  //     "espagnol": "Intercambios"
+  //   },
+  //   {
+  //     "frc": "Fictions",
+  //     "espagnol": "Ficciones"
+  //   },
+  //   {
+  //     "frc": "Réalités",
+  //     "espagnol": "Realidades"
+  //   },
+  //   {
+  //     "frc": "Innovations scientifiques",
+  //     "espagnol": "Innovaciones científicas"
+  //   },
+  //   {
+  //     "frc": "Responsabilités",
+  //     "espagnol": "Responsabilidades"
+  //   },
+  //   {
+  //     "frc": "Art",
+  //     "espagnol": "Arte"
+  //   },
+  //   {
+  //     "frc": "Pouvoir",
+  //     "espagnol": "Poder"
+  //   },
+  //   {
+  //     "frc": "Territoire",
+  //     "espagnol": "Territorio"
+  //   },
+  //   {
+  //     "frc": "Mémoire",
+  //     "espagnol": "Memoria"
+  //   },
+  //   {
+  //     "frc": "Diversité",
+  //     "espagnol": "Diversidad"
+  //   },
+  //   {
+  //     "frc": "Inclusion",
+  //     "espagnol": "Inclusión"
+  //   },
+  //   {
+  //     "frc": "Un robot",
+  //     "espagnol": "Un robot"
+  //   },
+  //   {
+  //     "frc": "Une créature fantastique",
+  //     "espagnol": "Una criatura fantástica"
+  //   },
+  //   {
+  //     "frc": "Les rencontres",
+  //     "espagnol": "Los encuentros"
+  //   },
+  //   {
+  //     "frc": "Un voyage dans le temps",
+  //     "espagnol": "Un viaje por el tiempo"
+  //   },
+  //   {
+  //     "frc": "Un mélange",
+  //     "espagnol": "Un mezcla"
+  //   },
+  //   {
+  //     "frc": "Autonome",
+  //     "espagnol": "Autónomo"
+  //   },
+  //   {
+  //     "frc": "Un bras",
+  //     "espagnol": "Un brazo"
+  //   },
+  //   {
+  //     "frc": "Un chirurgien, La chirurgie",
+  //     "espagnol": "Un cirujano, la cirugía"
+  //   },
+  //   {
+  //     "frc": "Agir",
+  //     "espagnol": "Actuar"
+  //   },
+  //   {
+  //     "frc": "Arriver",
+  //     "espagnol": "Llegar"
+  //   },
+  //   {
+  //     "frc": "Un risque",
+  //     "espagnol": "Un riesgo"
+  //   },
+  //   {
+  //     "frc": "États-Unis",
+  //     "espagnol": "EE.UU"
+  //   },
+  //   {
+  //     "frc": "Retourner → … à nouveau",
+  //     "espagnol": "Volver → Ha vuelto a..."
+  //   },
+  //   {
+  //     "frc": "Réussir à, obtenir",
+  //     "espagnol": "Lograr"
+  //   },
+  //   {
+  //     "frc": "L'entraide",
+  //     "espagnol": "La ayuda mutua"
+  //   },
+  //   {
+  //     "frc": "Le foie, hépatique",
+  //     "espagnol": "El hígado, hepático"
+  //   },
+  //   {
+  //     "frc": "Un poumon, pulmonaires",
+  //     "espagnol": "Un pulmón, pulmonares"
+  //   },
+  //   {
+  //     "frc": "Un rein, rénales",
+  //     "espagnol": "Un riñón, renales"
+  //   },
+  //   {
+  //     "frc": "Trouver",
+  //     "espagnol": "Encuentrar"
+  //   },
+  //   {
+  //     "frc": "Augmentation",
+  //     "espagnol": "Un incremento, un aumento"
+  //   },
+  //   {
+  //     "frc": "Don",
+  //     "espagnol": "Donación"
+  //   },
+  //   {
+  //     "frc": "Succès",
+  //     "espagnol": "Éxito"
+  //   },
+  //   {
+  //     "frc": "5 000, 2 000",
+  //     "espagnol": "Cinco mil, dos mil"
+  //   },
+  //   {
+  //     "frc": "357 392",
+  //     "espagnol": "trescientos cincuenta y siete mil cuatrocientos noventa y dos"
+  //   },
+  //   {
+  //     "frc": "30%",
+  //     "espagnol": "Treinta porciento"
+  //   },
+  //   {
+  //     "frc": "46,9",
+  //     "espagnol": "Cuarenta y seis coma nueve"
+  //   },
+  //   {
+  //     "frc": "Choisir",
+  //     "espagnol": "Escoger"
+  //   },
+  //   {
+  //     "frc": "La peau",
+  //     "espagnol": "La piel"
+  //   },
+  //   {
+  //     "frc": "Les jambes",
+  //     "espagnol": "Las piernas"
+  //   },
+  //   {
+  //     "frc": "Un chercheur",
+  //     "espagnol": "Un investigador"
+  //   },
+  //   {
+  //     "frc": "Le manque",
+  //     "espagnol": "La escasez"
+  //   },
+  //   {
+  //     "frc": "Une arbalette",
+  //     "espagnol": "Una baliesta"
+  //   },
+  //   {
+  //     "frc": "Une récolte",
+  //     "espagnol": "Una cosecha"
+  //   },
+  //   {
+  //     "frc": "La fumée",
+  //     "espagnol": "El humo"
+  //   },
+  //   {
+  //     "frc": "Abîmé",
+  //     "espagnol": "Estropeado"
+  //   },
+  //   {
+  //     "frc": "Bon pour la santé",
+  //     "espagnol": "Saludable"
+  //   },
+  //   {
+  //     "frc": "Substances dangereuses",
+  //     "espagnol": "Sustancias peligrosas"
+  //   },
+  //   {
+  //     "frc": "Les nouveaux nés",
+  //     "espagnol": "Los recién nacidos"
+  //   },
+  //   {
+  //     "frc": "Gaz à effet de serre",
+  //     "espagnol": "Gases de efecto invernadero"
+  //   }
+  // ]
+// }
+
+const voca =  {
+  "vocabulaire":[
+  {
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "abalanzarse",
+      "espagnol": "se jeter"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "acrecentar",
+      "espagnol": "augmenter"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "a diario",
+      "espagnol": "chaque jour"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "alcanzar",
+      "espagnol": "atteindre"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "amnistia",
+      "espagnol": "l'amnistie"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "a nado",
+      "espagnol": "la nage"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "ansiado",
+      "espagnol": "angoissé"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "añoranza",
+      "espagnol": "le regret"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "arrestos",
+      "espagnol": "les arrestations"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "balance",
+      "espagnol": "le bilan"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "balsas",
+      "espagnol": "les radeaux"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "balseros",
+      "espagnol": "ceux qui traversent une étendue d'eau en radeau"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "caída",
+      "espagnol": "la chute"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "cobrar",
+      "espagnol": "soutirer de l'argent"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "constituir",
+      "espagnol": "constituer"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "convertirse",
+      "espagnol": "se transformer"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "coyotes",
+      "espagnol": "les passeurs"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "crecimiento",
+      "espagnol": "la croissance"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "cruzar",
+      "espagnol": "traverser"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "el chicano",
+      "espagnol": "immigrant d'origine mexicaine"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "diario",
+      "espagnol": "le quotidien (journal)"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "documentación",
+      "espagnol": "les papiers"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "dueño",
+      "espagnol": "le patron"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "espaldas mojadas",
+      "espagnol": "les dos mouillés (clandestins traversant le Rio Grande à la nage)"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "focos",
+      "espagnol": "les foyers"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "franquear",
+      "espagnol": "franchir"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "hispanohablantes",
+      "espagnol": "hispanophones"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "hundimiento",
+      "espagnol": "l'effondrement"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "ilegales / los pollos",
+      "espagnol": "les clandestins / les sans-papiers d'identité"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "jerga",
+      "espagnol": "le jargon / le charabia"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "legalizar",
+      "espagnol": "légaliser"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "lograr",
+      "espagnol": "réussir"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "mapa",
+      "espagnol": "la carte"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "mezcla",
+      "espagnol": "un mélange"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "oleada",
+      "espagnol": "la grande vague"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "oriundo",
+      "espagnol": "originaire"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "petaca",
+      "espagnol": "la population"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "por barba",
+      "espagnol": "par tête de pipe"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "ranchero",
+      "espagnol": "le fermier"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "regresar",
+      "espagnol": "retourner"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "rotulado",
+      "espagnol": "dessiné"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "superar",
+      "espagnol": "surpasser"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "tamaño",
+      "espagnol": "la taille"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "tortilla",
+      "espagnol": "la galette de maïs"
+  },
+  {
+      "type": "traduction",
+      "languages": [
+          ":flag_fr:Francais:flag_fr:",
+          ":flag_es:Espagnol:flag_es:"
+      ],
+      "frc": "trasfondo",
+      "espagnol": "le problème de fond"
+  }
+]
 }
 
 
