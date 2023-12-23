85f3af8ed129e86585c7d8dde7ae9a88ebb38876
DeadMall0w <100719400+DeadMall0w@users.noreply.github.com>
2023-12-23 22:13:18 +0100

    move old files to archives

--- traduction-quiz.js
+++ traduction-quiz.js
@@ -32,8 +32,59 @@ function LoadQuestion(){
 }
 
 function SubmitAnswer(){
-  alert(inputText.value.toLowerCase());
+  let nbError = CheckAnswer(inputText.value, questions[questionIndex].langue2);
+  if (nbError == 0){
+    inputText.style.backgroundColor = '#2d7a4d'
+  }else if(nbError < 3){
+    inputText.style.backgroundColor = '#5d4102'
+  }else{
+    inputText.style.backgroundColor = '#5d0202'
+  }
 }
+
+
+function RandomNumber(min = 1, max = 10) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function LevenshteinDistance(s1, s2) {
+  if (s1.length < s2.length) {
+      return LevenshteinDistance(s2, s1);
+  }
+  if (s2.length == 0) {
+      return s1.length;
+  }
+  let previousRow = Array.from({length: s2.length + 1}, (_, i) => i);
+  for (let i = 0; i < s1.length; i++) {
+      let currentRow = [i + 1];
+      for (let j = 0; j < s2.length; j++) {
+          let insertions = previousRow[j + 1] + 1;
+          let deletions = currentRow[j] + 1;
+          let substitutions = previousRow[j] + (s1[i] != s2[j]);
+          currentRow.push(Math.min(insertions, deletions, substitutions));
+      }
+      previousRow = currentRow;
+  }
+  return previousRow[previousRow.length - 1];
+}
+
+function CheckAnswer(correctAnswer, userAnswer) {
+  userAnswer = userAnswer.toLowerCase();
+  correctAnswer = correctAnswer.toLowerCase();
+  if (correctAnswer == userAnswer) {
+      return 0;
+  }
+  if (correctAnswer.trim() == userAnswer.trim()) {
+      return 1;
+  }
+  if (userAnswer.includes(correctAnswer)) {
+      return 1;
+  } else {
+      return LevenshteinDistance(userAnswer, correctAnswer);
+  }
+}
+
 
 
 LoadQuiz("anglais.json");
